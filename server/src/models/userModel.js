61a0872f4741bd05383489a8e969c7e77ef36342
Shreyas Kumar Thakur <71756168+Shreyasthakur099@users.noreply.github.com>
2023-01-08 11:20:16 +0530

    Delete server directory

--- server/src/models/userModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const userModel = mongoose.Schema({
-    firstName : String,
-    lastName : String,
-    phoneNumber : Number
-})
-
-module.exports = mongoose.model("users" , userModel )
